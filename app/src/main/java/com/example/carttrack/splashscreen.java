c349369bb50fd3ec2b18e60da29977eb41305197
sarang <sarangspkach@gmail.com>
2021-09-29 13:57:05 +0530

    spalshscreen

---
+++ app/src/main/java/com/example/carttrack/splashscreen.java
@@ -0,0 +1,26 @@
+package com.example.carttrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashscreen extends AppCompatActivity {
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splashscreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
